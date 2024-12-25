49088a4bb505da6f7734e01884aa59a1c7a64ff0
Nik Uzair <nikuzairsc@gmail.com>
2024-12-25 13:47:18 +0800

    Initial Commit

---
+++ nickiess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nickiess
+//
+//  Created by Nik Uzair on 25/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ nickiessTests/nickiessTests.swift
@@ -0,0 +1,17 @@
+//
+//  nickiessTests.swift
+//  nickiessTests
+//
+//  Created by Nik Uzair on 25/12/2024.
+//
+
+import Testing
+@testable import nickiess
+
+struct nickiessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
